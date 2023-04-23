16f0a41938d2736725d0d36d9ee6749e0cc724c8
hami9m72 <21522332@gm.uit.edu.vn>
2023-04-23 21:32:59 +0700

    upload bottom-tab, tabs-navigator

---
+++ navigation/TabsNavigator.js
@@ -0,0 +1,191 @@
+import React from "react";
+import {Image, StyleSheet, View} from "react-native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "../screens/HomeScreen.js";
+import StatisticsScreen from "../screens/StatisticsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import BlogsScreen from "../screens/BlogsScreen.js";
+import AddScreen from "../screens/AddScreen.js";
+
+import {useTabMenu} from "../context/TabContext";
+
+const Tab = createBottomTabNavigator();
+
+const getIconColor = focused => ({
+  tintColor: focused ? "#6360DC" : "#000",
+});
+
+const TabsNavigator = () => {
+  const {opened, toggleOpened} = useTabMenu();
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBar,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarItemStyle: {
+            height: 0,
+          },
+          tabBarIcon: ({focused}) => (
+            <View style={styles.tabIconContainer}>
+              <Image
+                source={require("../image/Home.png")}
+                resizeMode="contain"
+                style={[styles.tabIcon, getIconColor(focused)]}
+              />
+            </View>
+          ),
+        }}
+        listeners={{
+          tabPress: e => opened && e.preventDefault(),
+        }}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={StatisticsScreen}
+        options={{
+          tabBarItemStyle: {
+            height: 0,
+          },
+          tabBarIcon: ({focused}) => (
+            <View style={styles.tabIconContainer}>
+              <Image
+                source={require("../image/Graph.png")}
+                resizeMode="contain"
+                style={[styles.tabIcon, getIconColor(focused)]}
+              />
+            </View>
+          ),
+        }}
+        listeners={{
+          tabPress: e => opened && e.preventDefault(),
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={AddScreen}
+        options={{
+          tabBarItemStyle: {
+            height: 0,
+          },
+          tabBarIcon: ({focused}) => (
+            <View style={styles.tabIconContainer}>
+              <Image
+                source={require("../image/Add.png")}
+                resizeMode="contain"
+                style={[styles.tabIcon, getIconColor(focused)]}
+              />
+            </View>
+          ),
+        }}
+        listeners={{
+          tabPress: e => opened && e.preventDefault(),
+        }}
+      />
+      <Tab.Screen
+        name="Blogs"
+        component={BlogsScreen}
+        options={{
+          tabBarItemStyle: {
+            height: 0,
+          },
+          tabBarIcon: ({focused}) => (
+            <View style={styles.tabIconContainer}>
+              <Image
+                source={require("../image/blogger.png")}
+                resizeMode="contain"
+                style={[styles.tabIcon, getIconColor(focused)]}
+              />
+            </View>
+          ),
+        }}
+        listeners={{
+          tabPress: e => opened && e.preventDefault(),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarItemStyle: {
+            height: 0,
+          },
+          tabBarIcon: ({focused}) => (
+            <View style={styles.tabIconContainer}>
+              <Image
+                source={require("../image/Setting.png")}
+                resizeMode="contain"
+                style={[styles.tabIcon, getIconColor(focused)]}
+              />
+            </View>
+          ),
+        }}
+        listeners={{
+          tabPress: e => opened && e.preventDefault(),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    padding: 0,
+    left: 16,
+    right: 16,
+    bottom: 32,
+    height: 56,
+    borderRadius: 16,
+    backgroundColor: "#fff",
+    borderTopColor: "transparent",
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 6,
+      width: 0,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+
+  tabBar2: {
+    position: "absolute",
+    padding: 0,
+    left: 16,
+    right: 16,
+    bottom: 32,
+    height: 56,
+    borderRadius: 16,
+    backgroundColor: "#fff",
+    borderTopColor: "transparent",
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 6,
+      width: 0,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+
+  tabIconContainer: {
+    position: "absolute",
+    top: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabIcon: {
+    width: 32,
+    height: 32,
+  },
+});
+
+export default TabsNavigator;
